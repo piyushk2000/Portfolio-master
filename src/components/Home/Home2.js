4a00a45fdb3a9bd903046804060bd79deb386962
Piyush <piyush.kalyani7@gmail.com>
2023-11-10 23:19:46 +0530

    change in resume

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,7 +87,7 @@ function Home2() {
               </li> */}
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/piyush-kalyani-a856a3194/"
+                  href="https://www.linkedin.com/in/piyushkalyani/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
