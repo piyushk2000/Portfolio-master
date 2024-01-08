f106e7a88854ccf50bff5b318ce63736f899742f
Piyush <piyush.kalyani7@gmail.com>
2024-01-08 22:38:34 +0530

    link change

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -32,8 +32,8 @@ function Projects() {
               title="VibeVault & VibeMatch"
               description="A personal creation: It's a two-in-one app. This MERN stack application allows users to rate and follow a wide range of media, and maintain a list, similar to MAL or IMDb, but for everything including books, anime, shows, and movies. 
               It also features VibeMatch, which uses a FastAPI backend to match users based on their media preferences it's like Tinder, but matchmaking is based on your media likes"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              ghLink="https://github.com/piyushk2000/vibeVault"
+              demoLink="https://vibe-vault-gamma.vercel.app/"
             />
           </Col>
 
@@ -46,7 +46,7 @@ function Projects() {
               title="Chat App"
               description=" Realtime chat app with React, Node.js ,socket.io , websocket, Mongodb and Express"
               ghLink="https://github.com/piyushk2000/chat-app"
-              demoLink="https://chat-app-three-virid.vercel.app/"
+              demoLink="https://vibe-vault-gamma.vercel.app/"
             />
           </Col>
 
@@ -57,7 +57,7 @@ function Projects() {
               title="E-Shop"
               description="Built an ecommerce platform using Django, React with, JWT authentication, Redux, and admin capabilities"
               ghLink="https://github.com/piyushk2000/django_shop"
-              demoLink="https://blogs.soumya-jit.tech/"
+              demoLink="https://django-shop-nine.vercel.app/#/"
             />
           </Col>
 
