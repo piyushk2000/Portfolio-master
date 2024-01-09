408db441808099433ee78cece8416370728cb4ea
Piyush <piyush.kalyani7@gmail.com>
2024-01-09 15:54:04 +0530

    phone view for video fixed

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,49 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.Test}
-        </Card.Text>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.id}
-        </Card.Text>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.pass}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+      {/* Conditionally render either an image or a video based on the presence of videoLink prop */}
+      {props.videoLink ? (
+        <div className="ratio ratio-16x9" style={{ marginBottom: "10px" }}>
+          <iframe src={props.videoLink} title="Project Video" allowFullScreen></iframe>
+        </div>
+      ) : (
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      )}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+      <Card.Body style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <div style={{ flex: "1 0 auto" }}>
+          <Card.Title>{props.title}</Card.Title>
+          {/* Show only the first 100 characters of the description */}
+          <Card.Text style={{ textAlign: "justify" }}>
+            {showMore ? props.description : `${props.description.slice(0, 150)}...`}
+          </Card.Text>
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+          {/* Show more button */}
+          {props.description.length > 100 && (
+            <Button variant="link" onClick={toggleShowMore}>
+              {showMore ? "Show Less" : "Show More"}
+            </Button>
+          )}
+
+          {/* Other Card.Text elements */}
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.Test}
+          </Card.Text>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.id}
+          </Card.Text>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.pass}
+          </Card.Text>
+        </div>
+
+        {/* Move GitHub and Demo buttons to the bottom */}
+        <div style={{ marginTop: "auto" }}>
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+
+          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component */}
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,13 +9,17 @@ import vibe from "../../Assets/Projects/vibe.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
-        
-      <div className="ratio ratio-16x9" style={{ width: "50%", height: "50%", margin: "auto", display: "block" ,marginBottom:"40px" }}>
-        <iframe src="https://www.youtube.com/embed/qepivEFTvwo?si=J2-M20IKJU_xDSVz" title="YouTube video player" allowFullScreen ></iframe>
-      </div>
+
+      <Row className="justify-content-center">
+        <Col xs={10} md={6}>
+          <div className="ratio ratio-16x9" style={{ width: "100%", height: "100%", margin: "auto", display: "block", marginBottom: "40px" }}>
+            <iframe src="https://www.youtube.com/embed/qepivEFTvwo?si=J2-M20IKJU_xDSVz" title="YouTube video player" allowFullScreen></iframe>
+          </div>
+        </Col>
+      </Row>
 
       <Particle />
-      <Container >
+      <Container style={{marginTop:"50px"}}>
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
@@ -29,6 +33,7 @@ function Projects() {
             <ProjectCard
               imgPath={vibe}
               isBlog={false}
+              // videoLink={"https://www.youtube.com/embed/qepivEFTvwo?si=J2-M20IKJU_xDSVz"}
               title="VibeVault & VibeMatch"
               description="A personal creation: It's a two-in-one app. This MERN stack application allows users to rate and follow a wide range of media, and maintain a list, similar to MAL or IMDb, but for everything including books, anime, shows, and movies. 
               It also features VibeMatch, which uses a FastAPI backend to match users based on their media preferences it's like Tinder, but matchmaking is based on your media likes"
